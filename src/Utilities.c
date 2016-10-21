807919c0aab45387e89962a7e71866d631301cb3
ermes1990 <ermes.franch@gmail.com>
2016-10-21 09:41:52 +0200

    commenti al file utitlities

--- src/Utilities.c
+++ src/Utilities.c
@@ -1,5 +1,10 @@
 #include "Utilities.h"
-
+/**
+ * Take as input an array its length and reverse it uo to the length
+ * for example: reverse([1,2,3,4,5] ,4) -> [4,3,2,1,5]
+ * @param *originale
+ * @param int length
+ */
 void reverse(uint8_t originale[],int length){ 					 		//take as input an array of Bytes and its length,  after this function is called your original vector will be reversed
 	uint8_t reversed[length];			                                                //array into which put a copy of the original one
 	int i;
@@ -11,6 +16,11 @@ void reverse(uint8_t originale[],int length){ 					 		//take as input an array o
 	}
 }
 
+/**
+ * save the 32bit integer t into the array uint8_t *t_bytes of length 4 
+ * @param uint32_t t
+ * @param uint8_t *t_Bytes
+ */
 void  int_To_Bytes(uint32_t t, uint8_t *t_Bytes){ 						 			//take as input an integer and an array of bytes  after this function is called, the array will be the reversed Bytes representation of the integer
     int i;
 	uint8_t *p=(uint8_t*) &t;                                                               //*p is a pointer uint8 representation  of the  input int t, but still reversed
@@ -20,6 +30,12 @@ void  int_To_Bytes(uint32_t t, uint8_t *t_Bytes){ 						 			//take as input an i
 	reverse(t_Bytes,4);                                                                         // reverse t_Bytes, now it is the 4 bytes rappresentation of int t with leftmost significant bit.
 }
 
+/**
+ * converts an array of bytes of max length 4 into an integer
+ * @param int len
+ * @param uint8_t *t_Bytes
+ * @return int res
+ */
 uint32_t Bytes_To_Int(int len, uint8_t  *t_Bytes){								// convert a stream of max 4 byte into an unsigned int with leftmost significant bit 
 	if(len > 4)
 	{
